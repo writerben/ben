2feb96e1ba93567dd6a4e986366afe0ad02d9c0e
writerben <64561235+writerben@users.noreply.github.com>
2020-08-06 12:38:53 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import Head from 'next/head'
 
 const Home = () => (
   <div>
-    <h1>Next.js on the [JAMstack](https://jamstack.org)</h1>
+    <h1>Covidification</h1>
 
-    <h3>Hooray 🎉 - you've built this with <a href="https://nextjs.org">Next.js</a>!</h3>
+    <h3>The changes Covid brought to this world</h3>
 
     <style jsx>{`
       :global(html,body) {
